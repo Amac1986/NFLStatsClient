89f85e6d6ae243db150f3f5121fa62a76b5bda38
Alex Mckenzie <Alexmckenzie25@gmail.com>
2022-01-03 09:18:15 -0600

    Machine switch WIP

--- NFLStats.Tests/ServicesTests/StatisticsServiceTests.cs
+++ NFLStats.Tests/ServicesTests/StatisticsServiceTests.cs
@@ -1,5 +1,13 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Newtonsoft.Json;
+using NFLStats.Model.Models;
 using NFLStats.Services.Interfaces;
+using NFLStats.Services.Services;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace NFLStats.Tests.ServicesTests
 {    
@@ -7,12 +15,38 @@ namespace NFLStats.Tests.ServicesTests
     public class StatisticsServiceTests
     {
         [TestMethod]
-        public void GetPagedRushingRecords_ReturnsRecordsSortedByYards_ByDefault_WhenArgumentsValid() 
+        public void GetPagedRushingRecords_ReturnsRecordsSortedByYards_WhenArgumentsValid() 
         {
-            var dataStore = new Moq.Mock<IDataStore>();
-            //dataStore.Setup(x => x.GetRushingRecords).Returns()
+            var sut = CreateBasicSUT();
+            var pageNumber = 1;
+            var sortBy = "Yards";
+            var playerFilter = "";
+
+
+            var records = sut.GetPagedRushingRecords(pageNumber, sortBy, playerFilter);
+
+            Assert.IsTrue(records.Any());
+            Assert.AreEqual(365, records.Count());
+
         }
 
-        //private List<RushingRecord> GetCollection
+        private StatisticsService CreateBasicSUT() 
+        {
+            IDataStore dataStore = Mock.Of<IDataStore>(d => d.GetRushingRecords() == GetCollectionRushingRecords());
+            var inMemorySettings = new Dictionary<string, string> {{ "ViewSettings:PageSize", "100"}};
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(inMemorySettings)
+                .Build();
+
+            return new StatisticsService(configuration, dataStore);
+        }
+
+        private IEnumerable<RushingRecord> GetCollectionRushingRecords() 
+        {
+            var path = "wwwroot/datafiles/rushing.json";
+
+            return JsonConvert.DeserializeObject<IEnumerable<RushingRecord>>(File.ReadAllText(path));
+        }
     }
 }
